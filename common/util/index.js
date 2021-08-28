8ed5f700948ebc57d9d218003bdfdb757913f4c6
10704302 <Wa@777888999>
2021-08-28 20:57:32 +0800

    update for ready

--- common/util/index.js
+++ common/util/index.js
@@ -1,6 +1,5 @@
 import {filteredEmptyImage, filteredEmptySong, formatPlaySongAlbumList, getSongInPlayListIndex, filteredPlaylistSongList, filteredSongEmptyImage} from './format';
-import {createParamRoute} from './route';
-import {navigationRoute} from './route';
+import {createParamRoute, navigationRoute} from './route';
 import {millisToMinutesAndSeconds} from './time';
 import {filteredSongEmptyFormatChecker} from './format-checker';
 
